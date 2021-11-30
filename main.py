814b5b60628f3ec63cc8846f7754d62b16fbaaf1
Gabriel Nitu <gabriel_nitu@yahoo.com>
2021-11-30 13:36:34 +0000

    Slider

--- main.py
+++ main.py
@@ -14,3 +14,7 @@ Aceasta este prima pagină web.
 ''')
 st.write(df)
 st.line_chart(df)
+st.area_chart(df)
+
+myslider = st.slider('Celsius')
+st.write(myslider, 'in Fahrenheit is ', myslider * 9/5 + 32)
